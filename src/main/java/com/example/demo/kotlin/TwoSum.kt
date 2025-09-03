719f3330dc380468442b8ca541b9a0fb7d8921a2
Anmol Chhabra <anmolpreet.chhabra01@fgfbrands.com>
2025-09-03 13:26:43 -0400

    Add TwoSum class skeleton with method signature and documentation

---
+++ src/main/java/com/example/demo/kotlin/TwoSum.kt
@@ -0,0 +1,15 @@
+package com.example.demo.kotlin
+
+class TwoSum {
+    /**
+     * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * You can return the answer in any order.
+     */
+    
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+            // Your solution will go here
+           
+    }  
+    
+}
